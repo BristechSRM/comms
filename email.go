b7f84f889bc9361d89923a1c27e63703b0aa819c
David Wybourn <dwybourn@scottlogic.com>
2016-03-09 04:06:54 -0800

    Addressing review comments. Removed unnecessary code and tests for getting last contacted date for a single or multi email addresses. Removed populating the last contacted map from the createRouter function. Removed unnecessary function level comments.

--- email.go
+++ email.go
@@ -2,61 +2,34 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"strings"
 )
 
 var lastContactedMap map[string]string
 
-const URL_PATH = "/email"
+const URL_PATH = "/last-contacted"
 const EMAIL_ADDRESS = "email-address"
 
-// main is the program's entry point.
 func main() {
+	populateLastContactedMap()
 	router := createRouter()
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-// createRouter creates a gorilla router that maps different paths/request types to
-// functions to handle those request.
 func createRouter() *mux.Router {
-	//TODO Currently populating hardcoded map. This is a side effect of this method that will be removed once we add database integration
-	populateLastContactedMap()
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc(URL_PATH, getAllLastContactedTimes).Methods("GET")
-	router.HandleFunc(fmt.Sprintf("%s/{%s}", URL_PATH, EMAIL_ADDRESS), getLastContactedTime).Methods("GET")
 
 	return router
 }
 
-// populateLastContactedMap creates a map of email addresses mapped
-// to a date in string format
 func populateLastContactedMap() {
 	lastContactedMap = make(map[string]string)
 	lastContactedMap["david.wybourn@superawesomegoodcode.co.uk"] = "2016-03-07T12:45:04Z"
 	lastContactedMap["chris.smith@leaddeveloper.com"] = "2016-02-17T15:51:15Z"
 	lastContactedMap["bob.builder@cartoonconstructionslimited.tv"] = "2004-01-30T05:00:01"
-}
-
-// getLastContactedTime gets the last contacted time for one or more email addresses.
-// The response is in json format of the form {emailAddress:date, ...}
-func getLastContactedTime(writer http.ResponseWriter, request *http.Request) {
-	vars := mux.Vars(request)
-	emailAddressesStr := vars[EMAIL_ADDRESS]
-
-	emailAddresses := strings.Split(emailAddressesStr, ",")
-
-	result := make(map[string]string)
-
-	for _, emailAddress := range emailAddresses {
-		result[emailAddress] = lastContactedMap[emailAddress]
-	}
-
-	writer.WriteHeader(http.StatusOK)
-	json.NewEncoder(writer).Encode(result)
 }
 
 // getAllLastContactedTimes gets the last contacted times for all of the email addresses.
--- email_test.go
+++ email_test.go
@@ -16,58 +16,14 @@ var (
 	url    string
 )
 
-// init is called before each test. It sets up the http test server
 func init() {
+	populateLastContactedMap()
 	server = httptest.NewServer(createRouter())
-	url = fmt.Sprintf("%s/email", server.URL)
+	url = fmt.Sprintf("%s/last-contacted", server.URL)
 }
 
-// TestGetLastContactedTimeSingleEmailAddress tests getting the last
-// contacted date for a single email address.
-func TestGetLastContactedTimeSingleEmailAddress(t *testing.T) {
-	emailAddress := "david.wybourn@superawesomegoodcode.co.uk"
-
-	response := performHttpRequest(emailAddress)
-	checkStatusOK(response.StatusCode, t)
-
-	responseMap := getResponseMap(response)
-
-	if len(responseMap) != 1 {
-		t.Errorf("Expected response size: 1, Actual response size: %d", len(responseMap))
-	}
-	if responseMap[emailAddress] != lastContactedMap[emailAddress] {
-		t.Errorf("Expected last contacted date: %s, Actual last contacted date: %s", lastContactedMap[emailAddress], responseMap[emailAddress])
-	}
-}
-
-// TestGetLastContactedTimeMultipleEmailAddresses tests getting the last
-// contacted date for multiple email addresses.
-func TestGetLastContactedTimeMultipleEmailAddresses(t *testing.T) {
-	emailAddresses := "david.wybourn@superawesomegoodcode.co.uk,chris.smith@leaddeveloper.com"
-
-	response := performHttpRequest(emailAddresses)
-	checkStatusOK(response.StatusCode, t)
-
-	responseMap := getResponseMap(response)
-
-	if len(responseMap) != 2 {
-		t.Errorf("Expected response size: 2, Actual response size: %d", len(responseMap))
-	}
-	// check each entry in the response map
-	for emailAddress := range responseMap {
-		contactedDate := lastContactedMap[emailAddress]
-		if contactedDate != responseMap[emailAddress] {
-			t.Errorf("Expected last contracted date to be %s, Actual date is: %s", contactedDate, responseMap[emailAddress])
-		}
-	}
-
-}
-
-// TestGetAllLastContactedTimes tests getting the last contacted date
-// for all email addresses.
 func TestGetAllLastContactedTimes(t *testing.T) {
-	// Passing no email address(es) does a GET all.
-	response := performHttpRequest("")
+	response := performHttpRequest()
 	checkStatusOK(response.StatusCode, t)
 
 	responseMap := getResponseMap(response)
@@ -80,7 +36,6 @@ func TestGetAllLastContactedTimes(t *testing.T) {
 	}
 }
 
-// getResponseMap creates a map from the http response.
 func getResponseMap(response *http.Response) map[string]string {
 	responseBody, err := ioutil.ReadAll(response.Body)
 	panicIfError(err)
@@ -91,10 +46,8 @@ func getResponseMap(response *http.Response) map[string]string {
 	return responseMap
 }
 
-// performHttpRequest performs a http request to our server with
-// the given email address(es) string and returns the response.
-func performHttpRequest(emailAddress string) *http.Response {
-	request, err := http.NewRequest("GET", fmt.Sprintf("%s/%s", url, emailAddress), strings.NewReader(""))
+func performHttpRequest() *http.Response {
+	request, err := http.NewRequest("GET", fmt.Sprintf("%s", url), strings.NewReader(""))
 	panicIfError(err)
 	response, err := http.DefaultClient.Do(request)
 	panicIfError(err)
