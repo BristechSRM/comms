d6237889e9f37bd7bfa677dd5ec6f7a049c8af3a
Lauren Clark <lclark@scottlogic.com>
2016-04-14 07:29:41 -0700

    Add CLIMutable

--- Comms/Entities.fs
+++ Comms/Entities.fs
@@ -3,17 +3,21 @@ namespace Comms.Entities
 open Amazon.DynamoDBv2.DataModel
 
 [<DynamoDBTable("Thread")>]
-type ThreadEntity() = 
-    member val Id = "" with get, set
+[<CLIMutable>]
+type ThreadEntity = {
+    Id : string
+}
     
 [<DynamoDBTable("CorrespondenceItem")>]
-type CorrespondenceEntity() =
-    member val Id = "" with get, set
-    member val SenderId = "" with get, set
-    member val ReceiverId = "" with get, set
-    member val Date = "" with get, set
-    member val Message = "" with get, set
-    member val Type= "" with get, set
-    member val SenderHandle = "" with get, set
-    member val ReceiverHandle = "" with get, set
-    member val ThreadId = "" with get, set
+[<CLIMutable>]
+type CorrespondenceEntity = {
+    Id : string
+    SenderId : string
+    ReceiverId : string
+    Date : string
+    Message : string
+    Type : string
+    SenderHandle : string
+    ReceiverHandle : string
+    ThreadId : string
+}
