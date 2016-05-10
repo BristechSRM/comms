85739c71d7dd8f4feeeb1135b1a9ec56026f85c6
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-05-10 13:41:43 +0100

    Remove type declarations

--- Comms/CorrespondenceController.fs
+++ Comms/CorrespondenceController.fs
@@ -11,12 +11,12 @@ open Comms.Entities
 type CorrespondenceController() = 
     inherit ApiController()
     
-    member x.Get(profileIdOne : string, profileIdTwo : string) = 
+    member x.Get(profileIdOne, profileIdTwo) = 
         Log.Information("Received get request for correspondence between profile 1: {profileIdOne} and profile 2 : {profileIdTwo}", profileIdOne, profileIdTwo)
         let correspondence = getCorrespondence profileIdOne profileIdTwo
         x.Request.CreateResponse(correspondence)
     
-    member x.Post(correspondence : CorrespondenceItemEntity) = 
+    member x.Post(correspondence) = 
         Log.Information("Received POST request for correspondence")
         let result = createCorrespondenceItem (correspondence)
         match result with
